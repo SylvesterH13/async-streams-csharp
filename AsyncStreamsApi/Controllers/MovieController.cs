df62c857dcec6c1392f86faf972b7bda24446bd1
SylvesterH13 <henrique.sylvester@gmail.com>
2022-10-05 09:23:37 -0300

    Implement get movies endpoint

---
+++ AsyncStreamsApi/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using AsyncStreamsApi.Model;
+using AsyncStreamsApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncStreamsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MovieController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpGet]
+        public IAsyncEnumerable<Movie> GetAsync()
+        {
+            return _movieRepository.GetAsync();
+        }
+    }
+}
