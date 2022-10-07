13f717ae8778cee8cf7698dbf246c2547accc8dc
SylvesterH13 <henrique.sylvester@gmail.com>
2022-10-06 22:48:38 -0300

    Implement MovieSqlRepository

--- AsyncStreamsApi/Program.cs
+++ AsyncStreamsApi/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IMovieRepository, MovieCsvRepository>();
+builder.Services.AddScoped<IMovieRepository, MovieSqlRepository>();
 
 var app = builder.Build();
 
---
+++ AsyncStreamsApi/Repositories/MovieSqlRepository.cs
@@ -0,0 +1,36 @@
+﻿using AsyncStreamsApi.Model;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace AsyncStreamsApi.Repositories
+{
+    public class MovieSqlRepository : IMovieRepository
+    {
+        private const int DELAY_IN_MILLISECONDS = 50;
+
+        private readonly string _connectionString;
+
+        public MovieSqlRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("LocalDB");
+        }
+
+        public async IAsyncEnumerable<Movie> GetAsync()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            var reader = await connection.ExecuteReaderAsync("SELECT * FROM Movies");
+
+            var rowParser = reader.GetRowParser<Movie>();
+
+            while (await reader.ReadAsync())
+            {
+                // To be able to see the effect of IAsyncEnumerable
+                await Task.Delay(DELAY_IN_MILLISECONDS);
+
+                yield return rowParser(reader);
+            }
+        }
+    }
+}
