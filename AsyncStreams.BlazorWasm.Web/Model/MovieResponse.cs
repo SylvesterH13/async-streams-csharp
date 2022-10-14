41fa327fcf58ef44a725e55335845f5070f81cd5
SylvesterH13 <henrique.sylvester@gmail.com>
2022-10-13 21:13:47 -0300

    Implement stream consuming in Blazor Wasm

---
+++ AsyncStreams.BlazorWasm.Web/Model/MovieResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncStreams.BlazorWasm.Web.Model
+{
+    public class MovieResponse
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public double Rating { get; set; }
+        public int RuntimeInMinutes { get; set; }
+    }
+}
